6e042f84aa882dd6657e0d24964bc10726d3d525
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-04-27 20:01:22 +0300

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle");
+
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    //  unused configuration commented out for now
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
