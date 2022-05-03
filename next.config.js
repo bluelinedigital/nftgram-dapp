9954343d4b0e3f8c7b96dee1f53181c4f26802bb
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-05-03 17:14:45 +0300

    web3-logic: Fixed images path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
   images: {
     loader: "akamai",
-    path: "https://nftgramm.com",
+    path: "/",
     domains: ["ipfs.infura.io"],
   },
 };
