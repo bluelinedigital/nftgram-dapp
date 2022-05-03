9567e42be3e43856a8443f97cce86b7b285b1f7e
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-05-03 14:19:16 +0300

    Fixed config dependepncies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  exportTrailingSlash: true,
+  trailingSlash: true,
   images: {
     loader: "akamai",
+    path: "https://nftgramm.com",
     domains: ["ipfs.infura.io"],
   },
 };
