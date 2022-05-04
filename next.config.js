9e5fb8d11da021b18bb5b2a0522afa669902962b
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-05-04 11:18:16 +0300

    web3-logic: Moved trailingSlash

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
     loader: "custom",
-    trailingSlash: true,
     domains: ["ipfs.infura.io"],
   },
 };
