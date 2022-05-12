47424eb19a303e0880287d833a7921303ebc6db2
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-05-12 16:37:15 +0300

    NOTASK smart contracts deployment, removed artifacts from gitignore

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftGramm = "0x7931baE0bA23E0CfF037bd9199Da16D19CF84481"
-  export const subContract = "0xBC12221cBC8bC47746c72392753C1f4FbC0e1217"
+  export const nftGramm = "0xC0F89F1A7044D7FC182Fa3F3434BECA4528D9e63"
+  export const subContract = "0x4Ae842395Ed14ed38B068f87b13297f6A941112d"
   
